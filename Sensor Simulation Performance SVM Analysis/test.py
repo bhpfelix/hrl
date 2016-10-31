9cf7b792a0cb0ccb5ade1b612b2b179bc0347f2f
bhpfelix <bhpfelix@gmail.com>
2016-10-31 11:34:58 -0400

    constraint ts > ta

--- Sensor Simulation Performance SVM Analysis/test.py
+++ Sensor Simulation Performance SVM Analysis/test.py
@@ -15,13 +15,13 @@ from forward_model import *
 total_time = [10]
 k_sens = 0.0349
 alpha_sens = 2.796*10**(-9)
-t_sens_0 = [30]
+t_sens_0 = [10, 30]
 t_amb = [25]
 k_obj = 0.15
 alpha_obj = 0.15/(440.*1660.)
 noise = [0.1, 1, 2]
 
-temp_models = [model_temperature(ts, ta, tt, k_sens, alpha_sens, k_obj, alpha_obj, n) for tt in total_time for ts in t_sens_0 for ta in t_amb for n in noise]
+temp_models = [model_temperature(ts, ta, tt, k_sens, alpha_sens, k_obj, alpha_obj, n) for tt in total_time for ts in t_sens_0 for ta in t_amb for n in noise if ta < ts]
 data = [model.run_simulation() for model in temp_models]
 for index, item in enumerate(data):
     time, temp_values = item
--- Sensor Simulation Performance SVM Analysis/util.py
+++ Sensor Simulation Performance SVM Analysis/util.py
@@ -1,1 +1,7 @@
 import pickle
+
+a = [15, 20, 25, 30]
+b = [25, 30, 35, 40]
+
+ab = [(i, j) for i in a for j in b if i < j]
+print ab
