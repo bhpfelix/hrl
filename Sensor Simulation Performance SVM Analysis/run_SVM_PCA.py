b3cda02bd68ef1f2d718cbdefbc7db7ae83c0951
bhpfelix <bhpfelix@gmail.com>
2016-11-10 10:00:38 -0500

    testing new svm

--- Sensor Simulation Performance SVM Analysis/run_SVM_PCA.py
+++ Sensor Simulation Performance SVM Analysis/run_SVM_PCA.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # SVM+PCA for Temperature Data
+from progressbar import *
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as pp
@@ -27,7 +28,9 @@ mat_path = "../simulation_confusion_mats"
 data_path = "../simulated_sensor_data"
 svm_path = "../trained_svms"
 pred_path = "../predictions"
+delta_e_path = "../delta_e"
 matsize = 420
+instances = 500
 exp = 20
 num_folds = 3
 total_time = [0.25, 0.5, 1.0, 2.0, 4.0]
@@ -48,6 +51,8 @@ for i in total_time:
         os.makedirs(os.path.join(svm_path,'%0.2f' % i))
     if not os.path.exists(os.path.join(pred_path,'%0.2f' % i)):
         os.makedirs(os.path.join(pred_path,'%0.2f' % i))
+    if not os.path.exists(os.path.join(delta_e_path,'%0.2f' % i)):
+        os.makedirs(os.path.join(delta_e_path,'%0.2f' % i))
 
 
 def create_dataset(mat, index, t_sens, t_amb, noise, dataset):
@@ -57,6 +62,29 @@ def create_dataset(mat, index, t_sens, t_amb, noise, dataset):
             dataset[key] = {'data': [], 'target': []}
         dataset[key]['data'] += [np.array(mat[i])]
         dataset[key]['target'] += [index]
+
+def create_binary_dataset(fmat1, fmat2, label1, label2, t):
+    data = {'data':[], 'target':[]}
+
+    for fvec in fmat1:
+        temp, slope = fvec
+        length = len(temp)
+        temp = temp[:int(t*length/MAX_TIME)]
+        slope = slope[:int(t*length/MAX_TIME)]
+        slope[-1] = 0
+        data['data'].append(temp + slope)
+        data['target'].append(str(label1))
+
+    for fvec in fmat2:
+        temp, slope = fvec
+        length = len(temp)
+        temp = temp[:int(t*length/MAX_TIME)]
+        slope = slope[:int(t*length/MAX_TIME)]
+        slope[-1] = 0
+        data['data'].append(temp + slope)
+        data['target'].append(str(label2))
+
+    return data
 
 def run_pca(dataset):
     # pca = decomposition.PCA(n_components=10)
@@ -118,14 +146,15 @@ def run_crossvalidation(mat, key, data_dict, folds, total_time):
     matrices[mat_key] = confusion_matrix_final
 
 
-def run_crossvalidation_new(data_dict, categories, folds):
+def run_crossvalidation_new(data_dict, folds):
     data_dict = run_pca(data_dict)
     skf = StratifiedKFold(data_dict['target'], n_folds=folds)
     svc = svm.SVC(kernel='linear')
     #svc = svm.SVC(kernel='rbf')
     #svc = svm.SVC(kernel='poly', degree=3)
-    scores = cross_val_score(svc, data_dict['data'], data_dict['target'], cv=skf)
+    scores = cross_val_score(svc, data_dict['data'], data_dict['target'], cv=skf, scoring='f1_weighted')
     print "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()/2)
+    return scores.mean()
 
 
 if __name__ == '__main__':
@@ -145,20 +174,62 @@ if __name__ == '__main__':
 
     for t in total_time:
         print 'Iterating through time %.2f' % t
-        matrices = {}
+        # matrices = {}
         for k in datatags:
             print 'Iterating through model %s' % str(k)
-            temp_data = {'data':[], 'target':[]}
+            # temp_data = {'data':[], 'target':[]}
+            trials = []
             for fname in datatags[k]:
                 dataVec = util.load_pickle(os.path.join(data_path, fname))
-                for pos,fvec in enumerate(dataVec):
-                    temp, slope = fvec
-                    length = len(temp)
-                    temp = temp[:int(t*length/MAX_TIME)]
-                    slope = slope[:int(t*length/MAX_TIME)]
-                    slope[-1] = 0
-                    temp_data['data'].append(temp + slope)
-                    temp_data['target'].append(str(pos))
+                trials.append(dataVec)
 
-            run_crossvalidation(matrices, k, temp_data, num_folds, t)
-        util.save_pickle(matrices, os.path.join(mat_path, 'confusion_matrices_%.2f.pkl' %t))
+            delta_e_results = []
+            for instance in range(instances):
+                print 'Iterating through instance %s' % instance
+
+                delta_e = 1
+
+                while True:
+                    print "Evaluating delta_e = %s" % delta_e
+                    prev = instance - delta_e
+                    next = instance + delta_e
+
+                    instanceVec = [dVec[instance] for dVec in trials]
+                    prevVec = None if prev < 0 else [dVec[prev] for dVec in trials]
+                    nextVec = None if next >= instances else [dVec[next] for dVec in trials]
+
+                    # Check if delta e exhausts the entire range
+                    if prevVec is None and nextVec is None:
+                        delta_e = 500
+                        break
+
+                    data_1 = create_binary_dataset(instanceVec, prevVec, instance, prev, t) if prevVec else None
+                    data_2 = create_binary_dataset(instanceVec, nextVec, instance, next, t) if nextVec else None
+
+                    score_1 = run_crossvalidation_new(data_1, num_folds) if data_1 else -1
+                    score_2 = run_crossvalidation_new(data_2, num_folds) if data_2 else -1
+
+                    mean_score = score_2 if score_1 < 0 else (score_1 if score_2 < 0 else (score_1+score_2)/2.)
+                    print "Score is %.2f" % mean_score
+
+                    if mean_score >= 0.9:
+                        break
+                    else:
+                        delta_e += 1
+
+                print "Delta_e = %s for instance %s" % (delta_e, instance)
+                delta_e_results.append(delta_e)
+
+            util.save_pickle(delta_e_results, os.path.join(delta_e_path, '%.2f'%t, '%s_%s_%s.pkl'%k))
+
+        #         for pos,fvec in enumerate(dataVec):
+        #             temp, slope = fvec
+        #             length = len(temp)
+        #             temp = temp[:int(t*length/MAX_TIME)]
+        #             slope = slope[:int(t*length/MAX_TIME)]
+        #             slope[-1] = 0
+        #             temp_data['data'].append(temp + slope)
+        #             temp_data['target'].append(str(pos))
+
+        #     run_crossvalidation(matrices, k, temp_data, num_folds, t)
+        # util.save_pickle(matrices, os.path.join(mat_path, 'confusion_matrices_%.2f.pkl' %t))
--- Sensor Simulation Performance SVM Analysis/test.py
+++ Sensor Simulation Performance SVM Analysis/test.py
@@ -14,5 +14,8 @@ from forward_model import *
 # ppp = util.load_pickle('../simulated_sensor_data/000_303.15_298.15_0.1.pkl')
 # print np.array(ppp).shape
 
-
-print max(1,[2,3])
+score_1 = 2
+score_2 = 1
+mean_score = score_2 if score_1 < 0 else (score_1 if score_2 < 0 else (score_1+score_2)/2.)
+k = (1,2,3)
+print "%s%s%s" % k
